0587a4fff3c724f34983b486f2d8c189c461e3cb
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2024-02-05 10:09:05 +0100

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="https://unsplash.com/photos/the-sun-is-setting-over-a-city-with-tall-buildings-87SqPLW-rfo" />
+        </nav>
+    )
+}
