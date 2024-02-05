14aa1e6d0efa9631456d33f4d3dbf2c00303702a
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2024-02-05 10:23:19 +0100

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components./Navbar.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <img src='.\assets\priscilla-du-preez-jRjHSce08Os-unsplash.jpg' />
+            <img src='.\assets\logo.jpg' />
         </nav>
     )
 }
