de49acdcc9fa2091d09705a0345ab6d5d75fb8f5
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2024-02-05 14:41:13 +0100

    img

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,7 +2,7 @@
 export default function Navbar() {
     return (
         <nav>
-            <img src="src/assets/logo.jpg" alt="Tesla Logo" />
+            <img src="src\assets\logo.jpg" alt="Tesla Logo" />
             <h1 className="heading1">The Tesla</h1>
         </nav>
     );
