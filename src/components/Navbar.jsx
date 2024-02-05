fde9eeca0184940436feab719521fd38d97e0522
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2024-02-05 10:21:42 +0100

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <img src="https://unsplash.com/photos/the-sun-is-setting-over-a-city-with-tall-buildings-87SqPLW-rfo" />
+            <img src='.\assets\priscilla-du-preez-jRjHSce08Os-unsplash.jpg' />
         </nav>
     )
 }
