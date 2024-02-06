646ae2f7209b7fa87a2c83298dbe16086405190f
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2024-02-06 10:09:49 +0100

    img

--- src/data/data.js
+++ src/data/data.js
@@ -1,32 +1,32 @@
  const tesla_data = [
   {
       "name": "Model S",
-      "image_url": "src\assets\model-s.jpg",
+      "image_url": "images\model-s.jpg",
       "description": "The Tesla Model S is a luxury all-electric sedan known for its performance and range."
   },
   {
       "name": "Model 3",
-      "image_url": "src/assets/model-3.jpg",
+      "image_url": "images\model-3.jpg",
       "description": "The Tesla Model 3 is a popular midsize all-electric sedan with a focus on affordability."
   },
   {
       "name": "Model X",
-      "image_url": "src/assets/model-x.jpg",
+      "image_url": "images\model-x.jpg",
       "description": "The Tesla Model X is an all-electric SUV featuring distinctive falcon-wing doors."
   },
   {
       "name": "Model Y",
-      "image_url": "src/assets/model-y.jpg",
+      "image_url": "images\model-y.jpg",
       "description": "The Tesla Model Y is a compact all-electric SUV with a sleek design and impressive performance."
   },
   {
       "name": "Cybertruck",
-      "image_url": "src/assets/Cybertruck.png",
+      "image_url": "images\Cybertruck.png",
       "description": "The Tesla Cybertruck is an all-electric pickup truck with a futuristic and robust design."
   },
   {
       "name": "Roadster",
-      "image_url": "src/assets/Roadster.jpg",
+      "image_url": "images\Roadster.jpg",
       "description": "The Tesla Roadster is a high-performance all-electric sports car, the first production car from Tesla."
   }
 ]
